e7ecf1a5ff8722f1092cc197ff5d49c0981ebbb0
chaowyc <1454744690@qq.com>
2015-10-24 10:05:21 -0300

    create delete show new comment tags

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaStreet::Application.config.secret_token = '799d9e8630fb93dfb1ea113fbafa7a6f896f0bec7ffbe01f6c92bf866ed35dd53306be254b9e2e4df6cd4b82dc8535ed4c82eeb7adb97da2c1a9070938fab869'
